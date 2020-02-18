11f6c37e03188b6834e0315e44127140071f8415
Japan Naik <Japan.Naik@iag.com.au>
2020-02-18 11:42:02 +1100

    Initial Commit

--- bin/template-core-app.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { TemplateCoreAppStack } from '../lib/template-core-app-stack';
-
-const app = new cdk.App();
-new TemplateCoreAppStack(app, 'TemplateCoreAppStack');
