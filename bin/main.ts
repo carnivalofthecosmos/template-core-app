76c4c5baab1c429770d733929b237fd2627e9f6f
Timothy Purchas <timothy.purchas@iag.com.au>
2020-02-26 09:42:50 +1100

    update libs for the bootstrap refactor

--- bin/main.ts
+++ bin/main.ts
@@ -4,26 +4,49 @@ import { App } from "@aws-cdk/core";
 import {
   ProjectStack,
   AccountStack,
-  CiEnvStack,
+  CiCdStack,
   EcsAppEnvStack
 } from "@carnivalofthecosmos/core";
 
 const app = new App();
 
+// Project Infra
 const projectStack = new ProjectStack(app, "Devops", {
   tld: "carnivalofthecosmos.com"
+  // env: { account: "1111" }
 });
 
+// Mgt Account Infra
 const mgtAccount = new AccountStack(projectStack, "Mgt", {
   cidr: "10.0.0.0/22"
 });
 
-const ciEnv = new CiEnvStack(mgtAccount, {
+// CiCd Infra
+const cicd = new CiCdStack(mgtAccount, {
   networkBuilder: mgtAccount.NetworkBuilder
 });
 
-const devEcsAppEnv = new EcsAppEnvStack(mgtAccount, "Dev", {
+// Dev Account Infra
+// const devAccount = new AccountStack(projectStack, "Dev", {
+//   cidr: "10.0.1.0/22",
+//   env: { account: "2222" }
+// });
+
+// Dev App Env Infra
+const devAppEnv = new EcsAppEnvStack(mgtAccount, "Dev", {
   networkBuilder: mgtAccount.NetworkBuilder
 });
 
-const tstAppEnv = new EcsAppEnvStack(mgtAccount, "Tst");
+// Tst App Env Infra
+const tstAppEnv = new EcsAppEnvStack(mgtAccount, "Tst", {
+  networkBuilder: mgtAccount.NetworkBuilder
+});
+
+// Prd Account Infra
+// const prdAccount = new AccountStack(projectStack, "Prd", {
+//   cidr: "10.0.2.0/22",
+//   env: { account: "3333" }
+// });
+
+// Prd App Env Infra
+// const prdAppEnv = new EcsAppEnvStack(prdAccount, "Prd");
